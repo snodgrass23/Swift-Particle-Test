18b72d453b28e14fec46f48829f0f5babc740895
Jim Snodgrass <jim@recurly.com>
2014-07-07 14:46:24 -0400

    initial commit

--- LyndaParticleEmitters/GameScene.swift
+++ LyndaParticleEmitters/GameScene.swift
@@ -11,31 +11,21 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
         
-        self.addChild(myLabel)
+        let snowEmmitterPath = NSBundle.mainBundle().pathForResource("snow", ofType: "sks")
+        let snow = NSKeyedUnarchiver.unarchiveObjectWithFile(snowEmmitterPath) as SKEmitterNode
+        snow.position = CGPointMake(size.width/2, size.height)
+        
+        addChild(snow)
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
    
